2ff52cd1b196f3b381d2c3391fd1f991a10e77c7
Mikael Walters <mikael.walters@gmail.com>
2017-11-15 20:34:51 -0600

    troubleshooting

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios.get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   };
+// };
+
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
