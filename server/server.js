a1a18b65de4102046d8f1a9b68b07ae30da28c77
Mikael Walters <mikael.walters@gmail.com>
2017-11-27 14:20:24 -0600

    saving to GitHub

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser")
 const keys = require("./config/keys");
 
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 
@@ -42,6 +43,7 @@ app.use(passport.session());
 //----------------------   Call authRoutes and immediately call 'app' function   ----------------------
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
